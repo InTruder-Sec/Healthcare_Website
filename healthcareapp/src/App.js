b75bae789d515eaebe6f11601493c7abbed58072
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-03-17 01:43:09 +0530

    Update

--- healthcareapp/src/App.js
+++ healthcareapp/src/App.js
@@ -1,24 +1,28 @@
 import './App.css';
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from './pages/home';
-import Gallery from './pages/gallery';
-import Blog from './pages/blog';
-import Testimonials from './pages/testimonials';
-import Services from './pages/services';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Gallery from './pages/Gallery';
+import Blog from './pages/Blog';
+import Testimonials from './pages/Testimonials';
+import Services from './pages/Services';
+import Videos from './pages/Videos';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
-    <>
+    <div className="App">
+      <NavBar />
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/gallery' element={< Gallery/>} />
-          <Route path='/blog' element={< Blog/>} />
-          <Route path='/testimonials' element={< Testimonials/>} />
-          <Route path='/services' element={< Services/>} />
+          <Route path="/" exact element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/testimonials" element={<Testimonials />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/videos" element={<Videos />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
