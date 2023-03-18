89d3eaf8dbf60afdefbb593266b20f3f551b4654
= <dhakatedeep909@gmail.com>
2023-03-18 09:25:04 +0530

    Blog updated

---
+++ healthcareapp/src/components/Blog/FeatureCards/FeatureCards.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./FeatureCard.css"
+
+function FeatureCard() {
+    return (
+        <div className="blog--feature--card">
+
+        </div>
+    )
+}
+
+export default FeatureCard;
